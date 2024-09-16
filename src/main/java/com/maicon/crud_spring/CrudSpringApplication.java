27a1e3ee427255e4036626f8007f6dd857fcc442
Maicon Cardoso <maiconscardoso@hotmail.com>
2024-09-16 07:25:31 -0300

    List of courses

--- src/main/java/com/maicon/crud_spring/CrudSpringApplication.java
+++ src/main/java/com/maicon/crud_spring/CrudSpringApplication.java
@@ -1,12 +1,34 @@
 package com.maicon.crud_spring;
 
+import com.maicon.crud_spring.model.Course;
+import com.maicon.crud_spring.repository.CourseRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-public class CrudSpringApplication {
+public class CrudSpringApplication implements CommandLineRunner {
+
+	private final CourseRepository courseRepository;
+
+	public CrudSpringApplication(CourseRepository courseRepository) {
+		this.courseRepository = courseRepository;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(CrudSpringApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		courseRepository.deleteAll();
+
+		Course course = new Course();
+		course.setName("Angular com Spring");
+		course.setCategory("front-end");
+
+		courseRepository.save(course);
 	}
 }
